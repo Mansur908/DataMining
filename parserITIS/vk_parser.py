76c0e797385ba16cf126db9b8feb5c0e2ed25065
Nurgaleev_11908 <55531380+Mansur908@users.noreply.github.com>
2021-03-14 00:25:55 +0300

    Add files via upload

--- parserITIS/vk_parser.py
+++ parserITIS/vk_parser.py
@@ -29,32 +29,9 @@ def execute_query(connection, query):
         print(f"The error '{e}' occurred")
 
 connection = create_connection(
-    "postgres", "postgres", "mansur1213", "127.0.0.1", "5432"
+    "vk_parser", "postgres", "mansur1213", "127.0.0.1", "5432"
 )
 
-
-
-
-
-map = {}
-count = 100
-offset = 0
-data = []
-rang = 300
-
-while offset < rang:
-    response = requests.get('https://api.vk.com/method/wall.get',
-                            params={
-                                'access_token': 'e2432b27e2432b27e2432b2753e235938bee243e2432b278203cc34da6f042f5f10ec79',
-                                'v': '5.130',
-                                'domain': 'itis_kfu',
-                                'count': count,
-                                'offset': offset
-                            }
-                            )
-    data1 = response.json()['response']['items']
-    offset += 100
-    data.extend(data1)
 
 def add(word):
     a = map.get(word)
@@ -68,12 +45,68 @@ def add_db(d):
         req = "INSERT INTO words(word,count) VALUES ('" + str(i) + "',"+str(d.get(i))+") ON CONFLICT(word) do update set count="+str(d.get(i))+";"
         execute_query(connection,req)
 
-for i in range(rang):
-    text = data[i]['text']
-    for j in re.split('\.|,|\'|\"|\s|«|»|-|\)|\(|/|:|0|1|2|3|4|5|6|7|8|9|/]|/}|—|⠀',text):
-        if j != '':
-            add(j)
-add_db(map)
+map = {}
+
+
+def start():
+    count = 100
+    offset = 0
+    data = []
+    rang = 300
+    while offset < rang:
+        response = requests.get('https://api.vk.com/method/wall.get',
+                                params={
+                                    'access_token': 'e2432b27e2432b27e2432b2753e235938bee243e2432b278203cc34da6f042f5f10ec79',
+                                    'v': '5.130',
+                                    'domain': 'itis_kfu',
+                                    'count': count,
+                                    'offset': offset
+                                }
+                                )
+        data1 = response.json()['response']['items']
+        offset += 100
+        data.extend(data1)
+
+    for i in range(rang):
+        text = data[i]['text']
+        for j in re.split('\.|,|\'|\"|\s|«|»|-|\)|\(|/|:|[0-9]|[a-z]|[A-Z]|/]|/}|—|⠀',text):
+            if j != '':
+                add(j)
+    add_db(map)
+
+from datetime import timedelta, datetime
+
+from airflow import DAG
+
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash import BashOperator
+from airflow.utils.dates import days_ago
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email': ['airflow@example.com'],
+    'start_date': datetime(2021, 3, 12),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1),
+}
+dag = DAG(
+    'parser',
+    default_args=default_args,
+    description='A simple tutorial DAG',
+    schedule_interval=timedelta(days=1),
+    start_date=days_ago(2),
+    tags=['example'],
+)
+
+run = PythonOperator(
+    task_id='test',
+    python_callable=start,
+    dag=dag,
+)
+
+run
 
 
 

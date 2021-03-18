80d9e174ffff983e22053e7efcd761c5cc1c041b
Nurgaleev_11908 <55531380+Mansur908@users.noreply.github.com>
2021-03-18 23:21:35 +0300

    Add files via upload

--- parserITIS/vk_parser.py
+++ parserITIS/vk_parser.py
@@ -74,7 +74,39 @@ def start():
                 add(j)
     add_db(map)
 
-start()
+from datetime import timedelta, datetime
+
+from airflow import DAG
+
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash import BashOperator
+from airflow.utils.dates import days_ago
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email': ['airflow@example.com'],
+    'start_date': datetime(2021, 3, 12),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1),
+}
+dag = DAG(
+    'parser',
+    default_args=default_args,
+    description='A simple tutorial DAG',
+    schedule_interval=timedelta(days=1),
+    start_date=days_ago(2),
+    tags=['example'],
+)
+
+run = PythonOperator(
+    task_id='test',
+    python_callable=start,
+    dag=dag,
+)
+
+run
 
 
 
